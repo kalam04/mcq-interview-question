55e5c213ec32e2891425006becf25848d5bab2f8
kalam04 <kalamgbcse@gmail.com>
2023-01-28 14:57:29 +0600

    add new login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,157 @@
+
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Sign In</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('/') }}assets/images/favicon.ico">
+
+    <!-- Layout config Js -->
+    <script src="{{ asset('/') }}assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('/') }}assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('/') }}assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('/') }}assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="{{ asset('/') }}assets/css/custom.min.css" rel="stylesheet" type="text/css" />
+
+    <link rel="stylesheet" href="{{ asset('/') }}assets/libs/toastr/toastr.min.css">
+    <link  href="{{ asset('/') }}assets/libs/select2/select2.min.css" rel="stylesheet" />
+
+
+</head>
+
+<body>
+
+    <div class="auth-page-wrapper pt-5">
+        <!-- auth page bg -->
+        <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
+            <div class="bg-overlay"></div>
+
+            <div class="shape">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
+                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+                </svg>
+            </div>
+        </div>
+
+        <!-- auth page content -->
+        <div class="auth-page-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center mt-sm-5 mb-4 text-white-50">
+                            <div>
+                                <a href="index.html" class="d-inline-block auth-logo">
+
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- end row -->
+
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card mt-4">
+
+                            <div class="card-body p-4">
+                                <div class="text-center mt-2">
+                                    <h5 class="text-primary">Please Fill Up Credential</h5>
+
+                                </div>
+                                <div class="p-2 mt-4">
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+
+                                        <div class="mb-3">
+                                            <label for="email" class="form-label">Email</label>
+                                            <input type="text" class="form-control" id="email" name="email" placeholder="Enter username" autofocus>
+                                        </div>
+
+                                        <div class="mb-3">
+
+                                            <label class="form-label" for="password-input">Password</label>
+                                            <div class="position-relative auth-pass-inputgroup mb-3">
+                                                <input type="password" class="form-control pe-5" placeholder="Enter password" id="password-input" name="password">
+                                                <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
+                                            <label class="form-check-label" for="auth-remember-check">Remember me</label>
+                                        </div>
+
+                                        <div class="mt-4">
+                                            <button class="btn btn-success w-100" type="submit">Log In</button>
+                                        </div>
+
+
+                                    </form>
+                                </div>
+                            </div>
+                            <!-- end card body -->
+                        </div>
+                        <!-- end card -->
+
+                    </div>
+                </div>
+                <!-- end row -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end auth page content -->
+
+        <!-- footer -->
+
+        @include('layouts.footer')
+        <!-- end Footer -->
+    </div>
+    <!-- end auth-page-wrapper -->
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('/') }}assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/node-waves/waves.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/feather-icons/feather.min.js"></script>
+    <script src="{{ asset('/') }}assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
+    <script src="{{ asset('/') }}assets/js/plugins.js"></script>
+
+    <!-- particles js -->
+    <script src="{{ asset('/') }}assets/libs/particles.js/particles.js"></script>
+    <!-- particles app js -->
+    <script src="{{ asset('/') }}assets/js/pages/particles.app.js"></script>
+    <!-- password-addon init -->
+    <script src="{{ asset('/') }}assets/js/pages/password-addon.init.js"></script>
+    <script src="{{ asset('/') }}assets/libs/jquery/jquery.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/toastr/toastr.min.js"></script>
+    <script src="{{ asset('/') }}assets/libs/select2/select2.min.js"></script>
+    {!! Toastr::message() !!}
+    <script type="text/javascript">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                toastr.error('{{ $error }}', 'Error', {
+                closeButton: true,
+                progressBar: true,
+                });
+            @endforeach
+        @endif
+
+    $(document).ready(function() {
+        $('.js-example-basic-single').select2();
+        $('.js-example-basic-multiple').select2();
+    });
+    </script>
+</body>
+
+</html>
